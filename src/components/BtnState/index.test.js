efd494a58b50c755395b8b7097471032a8f18cba
maycon santos <mayconzx1@gmail.com>
2023-05-29 14:43:46 -0300

    add new tests

--- src/components/BtnState/index.test.js
+++ src/components/BtnState/index.test.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import BtnState from '../BtnState'
+import { fireEvent, render } from '@testing-library/react-native';
 
 test("Snapshot correto", () => {
   const tree = renderer.create(<BtnState/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test("Renderizou corretamente", () => {
+  render(
+    <BtnState
+      color = "red"
+      text = "teste"
+      onPress = {() => {console.log("sucess")}}
+    />
+  );
+})
+
+test("Exibiu o texto correto", () => {
+  const {getByText} = render(
+    <BtnState
+      color = "red"
+      text = "testando texto"
+      onPress = {() => {console.log("sucess")}}
+    />
+  );
+
+  expect(getByText("testando texto")).toBeTruthy();
+})
+
+test("OnPress funcionando", () =>{
+  let a = 0;
+
+  const {getByText} = render(
+    <BtnState
+      color = "red"
+      text = "testando onpress"
+      onPress = {() => {a = 20}}
+    />
+  )
+  const btn = getByText("testando onpress")
+  fireEvent.press(btn);
+
+  expect(a).toBe(20);
+})
--- src/components/Pending/index.test.js
+++ src/components/Pending/index.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Pending from '../Pending'
+import { render } from '@testing-library/react-native';
 
 test("Snapshot correto", () => {
   const tree = renderer.create(<Pending/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test("Renderizou corretamente", () => {
+  render(
+    <Pending
+      msg = "teste"
+    />
+  );
+})
